114c44f17306b36f93ff2399c3413853ca0cc438
herbdool <herb@3speedhub.com>
2014-06-08 13:50:38 -0400

    Update Plugin.php

--- Classes/Plugin.php
+++ Classes/Plugin.php
@@ -1,21 +1,35 @@
 <?php
+/**
+ * Plugin class
+ */
+namespace Phile\Plugin\Herbdool\PhileDraft;
 
 /**
- * Class PhileDraft
+ * Class Plugin
+ *
+ * @author Herb v/d Dool
+ * @link http://github.com/herbdool/philedraft
+ * @license http://opensource.org/licenses/MIT
+ * @package Phile\Plugin\Phile\PhileDraft
+ * 
+ * From Zvonko Biškup's pico_draft plugin
+ * https://github.com/codeforest/pico_draft
  */
-class PhileDraft extends \Phile\Plugin\AbstractPlugin implements \Phile\Gateway\EventObserverInterface {
+class Plugin extends \Phile\Plugin\AbstractPlugin implements \Phile\Gateway\EventObserverInterface {
 	public function __construct() {
 		\Phile\Event::registerEvent('request_uri', $this);
 	}
 
 	public function on($eventKey, $data = null) {
-    // If the request_uri matches then it's content coming from Draft
+    	// If the request_uri matches then it's content coming from Draft
 		if (($eventKey == 'request_uri') && ($this->settings['draft_url'] == $data['uri'])) {
 			// Getting the payload, decoding it and saving to file system inside content dir
 			if ($_POST['payload']) {
 				// We have a request from Draft, let's save it to file
 				$payload = json_decode($_POST['payload']);
-				$fileName = strtolower($payload->name) . CONTENT_EXT;
+				$name = strtolower($payload->name);
+                                $name = str_replace(" ","-",$name);
+                                $fileName = $name . CONTENT_EXT;
 				@file_put_contents(CONTENT_DIR . $fileName, $payload->content);
 			}
 			exit;
